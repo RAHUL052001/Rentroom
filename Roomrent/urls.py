9141b4079b6601b39280c367289340c06126ef7a
RAHUL052001 <150319032+RAHUL052001@users.noreply.github.com>
2025-11-06 23:57:58 +0530

    Update urls.py
    stk

--- Roomrent/urls.py
+++ Roomrent/urls.py
@@ -4,8 +4,6 @@ from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
